ca1eab434249ca303e0e5ba54a1450157440340e
Perabjoth Singh Bajwa <perabjothsinghbajwa@gmail.com>
2024-08-24 00:39:42 -0400

    update build details

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/MassPoliceDiscipline/",
   plugins: [react()],
   build: {
     outDir: './docs',
     emptyOutDir: true, // also necessary
-  },
-  base: "/MassPoliceDiscipline/"
+  }
 })
